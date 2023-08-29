1a032a0622db2e7d715f2bac2d766fd7af358bc3
Lexis Hamilton <lexishamilton@datadrivendesign.net>
2023-08-29 07:28:53 -0700

    Adding existing files

--- plugins/clear-jobs/src/services/ClearJobsService.php
+++ plugins/clear-jobs/src/services/ClearJobsService.php
@@ -37,7 +37,7 @@ class ClearJobsService extends Component
             return $responseBody;
         } else {
 
-            // if empty, we wouldn't want to cache it.
+            // @TODO: if empty, we wouldn't want to cache it.sgit commit -m
             return [];
         }
     }
